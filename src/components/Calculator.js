c64029195ceca63b38b7cefe42e9c9cfa7b78972
yodit <yoditabebe9391gmail.com>
2023-03-22 17:14:24 +0300

    Add events on buttons for creating functionality

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import Result from './Result';
 import Input from './Input';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <div className="calculator">
-    <Result />
-    <Input />
-  </div>
-);
+const Calculator = () => {
+  const [state, setState] = useState({ total: '0', next: null, operation: null });
+  const onHandleClick = (e) => {
+    const targetText = e.target.textContent;
+    const newState = calculate(state, targetText);
+    setState(newState);
+  };
 
+  return (
+    <div className="calculator">
+      <Result state={state} />
+      <Input onHandleClick={onHandleClick} />
+    </div>
+  );
+};
 export default Calculator;
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,7 +1,22 @@
-const Result = () => (
-  <div className="result-cont">
-    <span className="result">0</span>
-  </div>
-);
+import PropTypes from 'prop-types';
+
+const Result = ({ state }) => {
+  const [total, next, operation] = [state.total, state.next, state.operation];
+  return (
+    <div className="result-cont">
+      <span className="result">
+        {total}
+        {' '}
+        {operation}
+        {' '}
+        {next}
+      </span>
+    </div>
+  );
+};
+
+Result.propTypes = {
+  state: PropTypes.objectOf(PropTypes.string).isRequired,
+};
 
 export default Result;
